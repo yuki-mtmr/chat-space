18a77de623bbb3050007c235cc2ea0c639892042
yuki-mtmr <yuki.matsumori.exam@gmail.com>
2019-10-24 12:34:36 +0900

    [update]メッセージ送信機能実装

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -2,8 +2,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
-  process resize_to_fit: [800, 800]
-
+  process resize_to_fit: [200, 200]
   # Choose what kind of storage to use for this uploader:
   storage :file
   # storage :fog
@@ -13,7 +12,6 @@ class ImageUploader < CarrierWave::Uploader::Base
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
-
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url(*args)
