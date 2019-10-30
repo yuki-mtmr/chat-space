f8e3406db08fbe6b38c139577d361d5b1abb300d
yuki-mtmr <yuki.matsumori.exam@gmail.com>
2019-10-30 14:20:05 +0900

    [update]自動更新

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,84 @@
+$(document).on('turbolinks:load', function(){
+  function buildHTML(message) {
+    var content = message.content ? `${ message.content }` : "";
+    var img  = message.image ? `<img class="message__text__image" src="${ message.image }">` :"";
+
+    var html = `<div class="message" data-message-id="${message.id}">
+                  <div class="message__upper-info">
+                    <div class="message__upper-info__talker">
+                      ${message.user_name}
+                    </div>
+                    <div class="message__upper-info__date">
+                      ${message.date}
+                    </div>
+                  </div>
+                    <div class="message__text">
+                      <p class="message__text__content">
+                        ${content}
+                      </p>
+                        ${img}
+                      </div>
+                   </div>`
+ 
+  return html;
+  }
+
+  //非同期通信
+
+  $(function(){
+    $('#new_message').on('submit', function(e){
+      e.preventDefault();
+      var formData = new FormData(this);
+      var url = $(this).attr('action');
+      $.ajax({
+        url: url,
+        type: 'POST',
+        data: formData,
+        dataType: 'json',
+        processData: false,
+        contentType: false
+      })
+      .done(function(message){
+        var html = buildHTML(message);
+        $('.messages').append(html)
+        $("#new_message")[0].reset();
+        $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
+
+      })
+      .fail(function(){
+        alert('error');
+      })
+    });
+  });
+
+  //自動更新機能
+  
+  var reloadMessages = function() {
+    if (window.location.href.match(/\/groups\/\d+\/messages/)) {
+      //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+      var last_message_id = $('.messages .message:last').data("message-id");
+
+      $.ajax({
+        //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
+        url: "api/messages",
+        //ルーティングで設定した通りhttpメソッドをgetに指定
+        type: 'get',
+        dataType: 'json',
+        //dataオプションでリクエストに値を含める
+        data: {id: last_message_id}
+      })
+      .done(function(messages) {
+        var insertHTML = '';
+          messages.forEach(function (message) {
+            insertHTML = buildHTML(message);
+            $('.messages').append(insertHTML);
+            $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
+          })
+      })
+      .fail(function() {
+        alert('自動更新に失敗しました');
+      });
+    };
+  };
+  setInterval(reloadMessages, 5000);
+});
--- app/views/messages/create.json.jbuilder
+++ app/views/messages/create.json.jbuilder
@@ -1,5 +1,6 @@
-json.id       @message.id
-json.content  @message.content
-json.image    @message.image.url
-json.name     @message.user.name
-json.date     @message.created_at.strftime("%Y/%m/%d %H:%M")
+json.content    @message.content
+json.image      @message.image.url
+json.date       @message.created_at.strftime("%Y/%m/%d %H:%M")
+json.user_name  @message.user.name
+#idもデータとして渡す
+json.id         @message.id
