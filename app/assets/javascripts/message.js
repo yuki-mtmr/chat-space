e45a98207167c37c20baac45feaa00edb9e5d654
yuki-mtmr <yuki.matsumori.exam@gmail.com>
2019-10-31 00:05:55 +0900

    Update message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -56,7 +56,7 @@ $(document).on('turbolinks:load', function(){
   var reloadMessages = function() {
     if (window.location.href.match(/\/groups\/\d+\/messages/)) {
       //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
-      var last_message_id = $('.messages .message:last').data("message-id");
+      var last_message_id = $('.message:last').data("message-id");
 
       $.ajax({
         //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
