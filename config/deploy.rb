35905f28250efe6e477d98840f8b7b93ad20ba32
yuki-mtmr <yuki.matsumori.exam@gmail.com>
2019-11-01 20:01:18 +0900

    update

--- config/deploy.rb
+++ config/deploy.rb
@@ -19,6 +19,7 @@ set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
 
+
 set :default_env, {
   rbenv_root: "/usr/local/rbenv",
   path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
